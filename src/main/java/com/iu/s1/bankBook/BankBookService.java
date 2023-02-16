15cb5aeec5a92cf7bb730680106846cb5ea27630
10000JI <sksk436@naver.com>
2023-02-16 15:53:43 +0900

    23-02-16

--- src/main/java/com/iu/s1/bankBook/BankBookService.java
+++ src/main/java/com/iu/s1/bankBook/BankBookService.java
@@ -2,8 +2,11 @@ package com.iu.s1.bankBook;
 
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.iu.s1.util.Pager;
 
@@ -12,6 +15,9 @@ public class BankBookService {
 	
 	@Autowired
 	private BankBookDAO bankBookDAO;
+	
+	@Autowired
+	private ServletContext servletContext;
 	
 	public List<BankBookDTO> getBankBookList(Pager pager) throws Exception {
 		Long totalCount = bankBookDAO.getBankBookCount(pager);
@@ -26,8 +32,12 @@ public class BankBookService {
 		return bankBookDAO.getBankBookDetail(bankBookDTO);
 	}
 	
-	public int setBankBookAdd(BankBookDTO bankBookDTO) throws Exception{
-		return bankBookDAO.setBankBookAdd(bankBookDTO);
+	public int setBankBookAdd(BankBookDTO bankBookDTO, MultipartFile pic) throws Exception{
+		//1. File을 HDD에 저장
+		
+		System.out.println(servletContext);
+		
+		return 0; //bankBookDAO.setBankBookAdd(bankBookDTO);
 	}
 	
 	public int setBankBookUpdate(BankBookDTO bankBookDTO) throws Exception{
---
+++ src/main/java/com/iu/s1/util/FileManager.java
@@ -0,0 +1,15 @@
+package com.iu.s1.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileManager {
+	
+	//File을 HDD에 저장
+	public void fileSave(MultipartFile file) throws Exception {
+		//1. 어디에 저장할 것인가?
+		//	/resources/upload/bankBook/...
+		
+		//2. 저장관리는 운영체제가 담당
+		
+	}
+}
