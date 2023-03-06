4108df61f934a489113ccc177450e11daef6837b
10000JI <sksk436@naver.com>
2023-03-06 11:30:35 +0900

    230306 ( js - ajax )

--- src/main/webapp/resources/js/bankBookReply.js
+++ src/main/webapp/resources/js/bankBookReply.js
@@ -1,6 +1,7 @@
 const replyAdd = document.getElementById("replyAdd");
 const replyContents = document.getElementById("replyContents");
 const commentListResult = document.getElementById("commentListResult");
+//const pageLink = document.querySelectorAll(".page-link");
 
 replyAdd.addEventListener("click",function(){
     let xhttp = new XMLHttpRequest();
@@ -28,18 +29,80 @@ replyAdd.addEventListener("click",function(){
      })
 })
 
-getList();
+//bookNum으로 댓글목록
+getList(1);
+function getList(page){
 
-function getList(){
+    let count = 0;
+
     let xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", "/bankBookComment/list?bookNum="+replyAdd.getAttribute('data-book-bookNum'))
+    xhttp.open("GET", "/bankBookComment/list?bookNum="+replyAdd.getAttribute('data-book-bookNum')+"&page="+page)
 
     xhttp.send();
 
     xhttp.addEventListener("readystatechange", function(){
         if(this.readyState==4 && this.status==200){
             commentListResult.innerHTML = this.responseText.trim();
+            count ++;
         }
     })
+    
+    //0이 출력 : 비동기 방식이기 때문에
+    console.log("count :"+count);
 }
+
+//page요청할 수 있도록 만든 이벤트
+commentListResult.addEventListener("click",function(e){
+    let pageLink = e.target;
+    if(pageLink.classList.contains("page-link")){
+        let page = pageLink.getAttribute('data-board-page');
+        getList(page);
+    }
+
+    e.preventDefault();
+
+})
+
+
+commentListResult.addEventListener('click',function(e){
+
+    let deleteButton = e.target;
+    if(deleteButton.classList.contains("del")){
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "../bankBookComment/delete");
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("num="+deleteButton.getAttribute("data-comment-num"))
+        xhttp.addEventListener("readystatechange", function(){
+            if(this.readyState==4 && this.status==200){
+               let result = this.responseText.trim();
+               if(result==1){
+                alert("댓글이 삭제 되었습니다.");
+                getList(1);
+               } else{
+                alert("삭제 실패");
+               }
+            }
+        })
+
+    }
+    e.preventDefault();
+})
+
+
+commentListResult.addEventListener('click',function(e){
+
+    let updateButton = e.target;
+    if(updateButton.classList.contains("update")){
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "../bankBookComment/update?num="+updateButton.getAttribute("data-update-num"));
+        xhttp.send()
+        xhttp.addEventListener("readystatechange", function(){
+            if(this.readyState==4 && this.status==200){
+              console.log(this.responseText)
+            }
+        })
+
+    }
+    e.preventDefault();
+})
