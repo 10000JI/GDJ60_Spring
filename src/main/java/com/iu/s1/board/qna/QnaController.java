95e5d32e876f38bb704f3476a048f3ddbe90ac48
10000JI <sksk436@naver.com>
2023-03-10 11:45:59 +0900

    20230310

--- src/main/java/com/iu/s1/board/qna/QnaController.java
+++ src/main/java/com/iu/s1/board/qna/QnaController.java
@@ -1,18 +1,23 @@
 package com.iu.s1.board.qna;
 
+import java.net.http.HttpHeaders;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,6 +50,38 @@ public class QnaController {
 		List<BbsDTO> ar = qnaService.getBoardList(pager);
 		mv.addObject("list", ar);
 		mv.setViewName("board/list");
+		
+		//RestAPI ex>카카오로그인
+		RestTemplate restTemplate = new RestTemplate();
+		
+		//URL, Method, parameter, header
+		
+		//Header
+		HttpHeaders headers = new HttpHeaders(); 
+		//1. headers.add("header명", 'header의 값');
+		headers.add("Content-Type", "application/x-www-form-urlencoded")
+		//2.headers.set헤더명("값");
+		headers.setContentType();
+		
+		//parameter(post)
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+		params.add("파라미터명", "파라미터값");
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "${REST_API_KEY}");
+		
+		//header와 params 하나의 객체로 생성
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String,String>>(params,headers);
+		
+		//URL
+		//get방식
+		//String result = restTemplate.getForObject("https://dummyjson.com/products/1", String.class, request);
+		
+		//URL
+		//get방식
+		String result = restTemplate.postForObject("https://dummyjson.com/products/1", request, String.class);
+		
+		
+		System.out.println(result);
 		return mv;
 	}
 	
